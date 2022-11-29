56bae11e29b56560be4ff6301ead7da729d325d6
Petru <109862244+pbunaciu@users.noreply.github.com>
2022-11-29 21:29:25 +0200

    one function cleaned out, do further tests at school

---
+++ get_next_line_V2.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbunaciu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 07:57:13 by pbunaciu          #+#    #+#             */
+/*   Updated: 2022/11/29 16:10:23 by pbunaciu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*find_nextline(char *stash)
+{
+	int		i;
+	int		j;
+	char	*line_ret;
+
+	i = 0;
+	j = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free (stash);
+		stash = NULL;
+		return (NULL);
+	}
+	line_ret = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!line_ret)
+		return (NULL);
+	i++;
+	while (stash[i])
+		line_ret[j++] = stash[i++];
+	free (stash);
+	stash = NULL;
+	line_ret[j] = '\0';
+	return (line_ret);
+}
+
+char	*find_newline(char *stash)
+{
+	int		j;
+	char	*line_ret;
+
+	j = 0;
+	if (!stash[j])
+	{
+		return (NULL);
+	}
+	while (stash[j] && stash[j] != '\n')
+			j++;
+	line_ret = malloc(sizeof(char) * (j + 2));
+	if (!line_ret)
+		return (NULL);
+	j = 0;
+	while (stash[j] && stash[j] != '\n')
+	{
+		line_ret[j] = stash[j];
+		j++;
+	}
+	if (stash[j] && stash[j] == '\n')
+		line_ret[j++] = '\n';
+	line_ret[j] = '\0';
+	return (line_ret);
+}
+
+char	*read_save(int fd, char *stash)
+{
+	char	*buf;
+	int		read_byte;
+    char    *temp;
+
+	read_byte = 1;
+	if (!stash)
+		stash = ft_calloc(1, 1);
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	while ((read_byte = read(fd, buf, BUFFER_SIZE)) > 0)
+    {
+		buf[read_byte] = '\0';
+	    temp = strjoin(stash, buf);
+        free (stash);
+        stash = temp;
+		if (ft_strchr(stash, '\n'))
+		    break ;
+	}
+	free (buf);
+	if (read_byte == -1)
+		return (NULL);
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line_ret;
+	static char	*stash;
+
+	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = (read_save(fd, stash));
+	if (!stash)
+		return (NULL);
+	line_ret = find_newline(stash);
+	stash = find_nextline(stash);
+	return (line_ret);
+}
